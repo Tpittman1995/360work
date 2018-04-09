994f8c8f5af06aa9e375e9e507c954d0152aeadc
Travis Pittman <travis1045@live.com>
2018-04-09 10:40:57 -0700

    added check if empty to rmdir.c

--- lab6/rmdir.c
+++ lab6/rmdir.c
@@ -5,6 +5,43 @@ extern int  n;            // number of token strings
 extern PROC *running;
 extern int dev;
 
+
+int check_if_empty(MINODE * mip, char * pathname)
+{
+	INODE *tempip = &(mip->INODE);
+	int number = 0;
+	char buff[BLKSIZE];
+	char temp[256];
+	char *cp;
+	char *s;
+
+
+		for (int i=0; i<12; i++){ 
+
+    		if (tempip->i_block[i]==0) break;
+
+    		
+    		get_block(dev, tempip->i_block[i], buff);
+
+    		cp = buff;
+    		dp = (DIR *)buff;
+    		
+
+    	while(cp<buff+1024){
+    		strncpy(temp, dp->name, dp->name_len);
+       		temp[dp->name_len] = 0;
+  
+       		//printf("%s\n", dp->name);
+       		number++;
+
+       		cp += dp->rec_len;
+       		dp = (DIR *)cp;
+    	}
+    	}
+	
+    	return number;
+}
+
 int my_rmdir(char* path){
 	printf("Hello World: %s\n", path);
 	int ino = getino(&dev, path);
@@ -19,11 +56,11 @@ int my_rmdir(char* path){
   		printf("Directory is Busy\n");
   		return 0;
   	}
-  	else if(mip->INODE.i_links_count > 2)
+  	else if(check_if_empty(mip, path) > 2)
   	{
   		printf("Directory is not empty\n");
   	}
-  	//else if()
+  	
 	tokenize(path);
 	printf("tokenize: %s %d\n", name[1], n);
 }
